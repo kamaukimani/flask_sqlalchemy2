eafa087d7dbbc5ba110790b19d2d67b3234a11c7
kamaukimani <paulkimys285@gmail.com>
2026-09-15 14:29:05 +0300

    set up Game model

---
+++ app/models/game.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped,mappped_column,relationship
+from app.db import db
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+class Game(db.Model,SerializerMixin):
+    __tablename__="games"
+
+    id:Mapped[int]=mapped_column(primary_key=True)
+    title:Mapped[str]
+    genre:Mapped[str]
+    platform:Mapped[str]
+    price:Mapped[int]
+    created_at:Mapped[datetime]=mapped_column(server_default=func.now())
+    updated_at:Mapped[datetime]=mapped_column(server_default=func.now(),onupdate=func.now())
