313151411252f280662f72a1dce1f005f7202147
kamaukimani <paulkimys285@gmail.com>
2026-09-15 15:28:37 +0300

    add the create_app() function

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from .db import db,migrate
+from .config import Config
+ 
+
+def create_app():
+    app=Flask(__name__)
+
+    app.config.from_object(Config)
+    app.json.compact=False
+
+    migrate.init_app(app,db)
+
+    db.init_app(app)
+
+    
+    return app
+
