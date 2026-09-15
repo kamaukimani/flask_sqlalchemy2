a9dcdd39ce8f7dde642ae0a194138d3a8f106380
kamaukimani <paulkimys285@gmail.com>
2026-09-15 15:32:19 +0300

    correct import name

--- app/db.py
+++ app/db.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate 
-from sqlalchemy import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
