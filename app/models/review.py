c7f6b4ad73179cce82e79721264a0b725d76b265
kamaukimani <paulkimys285@gmail.com>
2026-09-16 14:09:14 +0300

    remove primary key constraint in foreignkey column

--- app/models/review.py
+++ app/models/review.py
@@ -17,8 +17,8 @@ class Review(db.Model,SerializerMixin):
     created_at:Mapped[datetime]=mapped_column(server_default=func.now())
     updated_at:Mapped[datetime]=mapped_column(server_default=func.now(),onupdate=func.now())
     
-    game_id:Mapped[int]=mapped_column(ForeignKey("games.id"),primary_key=True)
-    user_id:Mapped[int]=mapped_column(ForeignKey("users.id"),primary_key=True)
+    game_id:Mapped[int]=mapped_column(ForeignKey("games.id"))#,primary_key=True)
+    user_id:Mapped[int]=mapped_column(ForeignKey("users.id"))#,#primary_key=True)
 
     game:Mapped["Game"]=relationship(back_populates="reviews")
     user:Mapped["User"]=relationship(back_populates="reviews")
